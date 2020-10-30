dd4054720025bdd4aca8a017eba16e4af93712df
dreamcode <dreamcode@192.168.10.28>
2020-10-30 17:53:55 -0500

    Act

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -22,28 +22,28 @@ public class BaseClass {
 	@BeforeTest
 	public void setup() {
 		
-		try {
-	
+		try {			
+			
 		DesiredCapabilities caps = new DesiredCapabilities();
-		
+
 		//caps.setCapability("platformName", "ANDROID");
 		caps.setCapability(CapabilityType.PLATFORM_NAME, "ANDROID");
-		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1.0");
-		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "SM-G610M");
-		caps.setCapability(MobileCapabilityType.UDID, "3300f6599518944f");
+		caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "sdk_gphone_x86_arm");
+		//caps.setCapability(MobileCapabilityType.UDID, "3300f6599518944f");
 		caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
-		File file = new File("C:\\Users\\kjosi\\Documents\\Automatización Bienestar Seguros Bolivar", "balm-444.apk");
+		File file = new File("/Users/dreamcode/eclipse-workspace/AppBienestarSegurosBolivar/src/test/resources/app", "bolivar-conmigo-V001.apk");
 		caps.setCapability(MobileCapabilityType.APP, file.getAbsolutePath());
 		//caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-		
-		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");	
 		
 		//driver = new AppiumDriver<MobileElement>(url, caps);
 		driver = new AndroidDriver<MobileElement>(url, caps);
 		//driver = new IOSDriver<MobileElement>(url, caps);
 		
 		}catch(Exception exp){
-			System.out.println("Falló porque: "+exp.getCause());
+			System.out.println("Fallo porque: "+exp.getCause());
 			System.out.println("Mensaje es: "+exp.getMessage());
 			exp.printStackTrace();
 		}
